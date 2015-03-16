8577562e5d70c243831793286b7ff129af0894e0
michael.lake <michael.lake@willowtreeapps.com>
2015-03-16 16:41:43 -0400

    Fixing menu button when navigating to other pages

--- main.js
+++ main.js
@@ -71,6 +71,14 @@ function createIconForRow(row) {
 
 
 $(document).ready(function(){
+    /*  Hamburger Menu & Icon  */
+    $('.hamburger').on('click', function(e){
+
+        e.preventDefault();
+        $(this).toggleClass('opned');
+        $('header nav').toggleClass('active');
+
+    });
 
 
     var businessTemplate = Handlebars.compile($('#business_template').html());
@@ -145,42 +153,6 @@ $(document).ready(function(){
         sql: 'select * order by %Name%',
         userCallback: myUserCallback
     });
-
-
-
-
-	/*  Hamburger Menu & Icon  */
-	$('.hamburger').on('click', function(e){
-		
-		e.preventDefault();
-		$(this).toggleClass('opned');
-		$('header nav').toggleClass('active');
-		
-	});
-
-
-
-
-	/*  Advanced search form & Icon  */
-	$('#advanced_search_btn').on("click", function(e){
-		e.preventDefault();
-
-		var ads_box =$('.advanced_search');
-		
-		if(!ads_box.hasClass('advanced_displayed')){
-
-			$(this).addClass('active');
-			ads_box.stop().fadeIn(200).addClass('advanced_displayed');
-
-		}else{
-
-			$(this).removeClass('active');
-			ads_box.stop().fadeOut(200).removeClass('advanced_displayed');
-
-		}
-
-	});
-
 
 
 
